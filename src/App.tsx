154de4e8fa50ec1afcaf8a4ac4afb427cc9ca9b7
abhijitroy0356 <60839264+abhijitroy0356@users.noreply.github.com>
2025-03-04 21:44:22 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <Todo title="Abhijit" description='idk' done={false}/>
+       
+    </>
+  )
+}
+interface Todoprop{
+  title:string;
+  description:string;
+  done:boolean;
+
+}
+
+function Todo(props:Todoprop){
+    return (
+      <div>
+        <div>{props.title}</div>
+        <div>{props.description}</div>
+        <div>{props.done}</div>
+        
+      </div>
+    )
+}
+enum keys{
+  Up,Down,Left,Right
+}
+function doshit(mousemove:keys):void{
+  if(mousemove==keys.Down){
+      console.log('down')
+  }
+}
+doshit(keys.Down)
+function getelelmet<T>(arr:T[]):T{
+  return arr[0];
+}
+let vall=getelelmet(['aH','10'])
+let vall1=getelelmet([1,2])
+let vall2=getelelmet([true,false])
+console.log(vall.toUpperCase());
+
+export default App
